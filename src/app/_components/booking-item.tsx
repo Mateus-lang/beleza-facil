5c1e32b8d6df8182b0d2c34bcffc0058557f6207
[Mateus] <143464157+Mateus-lang@users.noreply.github.com>
2026-02-16 23:21:41 -0300

    feat: add Avatar component and update BookingItem to include avatar display

--- src/app/_components/booking-item.tsx
+++ src/app/_components/booking-item.tsx
@@ -1,3 +1,4 @@
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 import { Badge } from './ui/badge';
 import { Card } from './ui/card';
 
@@ -15,16 +16,32 @@ const BookingItem = ({
   date,
 }: BookingItemProps) => {
   return (
-    <div>
-      <Card>
-        <div className="flex flex-col gap-2 px-3 py-2">
-          <Badge className="bg-green-50 text-sm text-green-700 dark:bg-green-950 dark:text-green-300">
-            Confirmado
-          </Badge>
-          <p className="text-sm">{serviceName}</p>
+    <Card className="flex flex-row items-center justify-between p-4">
+      <div className="flex flex-col gap-2">
+        <Badge className="bg-green-50 text-sm text-green-700 dark:bg-green-950 dark:text-green-300">
+          Confirmado
+        </Badge>
+        <div className="flex flex-col gap-2">
+          <p className="text-sm font-bold">{serviceName}</p>
+          <div className="flex items-center gap-2">
+            <Avatar>
+              <AvatarImage src={salonImageUrl} alt={salonName} />
+            </Avatar>
+            <p className="text-sm">{salonName}</p>
+          </div>
         </div>
-      </Card>
-    </div>
+      </div>
+
+      <div className="flex h-full flex-col items-center justify-center border-l p-4 py-3">
+        <p className="text-xs capitalize">
+          {date.toLocaleDateString([], { month: 'long' })}
+        </p>
+        <p className='text-2xl'>{date.toLocaleDateString([], { day: '2-digit' })}</p>
+        <p className="text-xs">
+          {date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}
+        </p>
+      </div>
+    </Card>
   );
 };
 
